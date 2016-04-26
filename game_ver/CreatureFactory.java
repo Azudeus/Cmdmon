c31f0583409968d4c8f2f9cd98466bbd3f413ebb
lolipopper <njruntuwene@gmail.com>
2016-04-27 01:49:21 +0900

    add CreatureFactory

---
+++ game_ver/CreatureFactory.java
@@ -0,0 +1,25 @@
+/**
+*@author Nathan James Runtuwene/13514083
+*/
+
+public class CreatureFactory {
+	public static Creature createCreature(String what,int multiplier)
+	{
+		if (what.equals("Harpy"))
+		{
+			return new Harpy(row, column, directionX, directionY, multiplier);
+		}
+		else if (what.equals("Lamia"))
+		{
+			return new Lamia(row, column, directionX, directionY, multiplier);
+		}
+		else if (what.equals("Centaur"))
+		{
+			return new Centaur(row, column, directionX, directionY, multiplier);
+		}
+		else if (what.equals("Plant"))
+		{
+			return new Plant(row, column, multiplier);
+		}
+	}
+}
